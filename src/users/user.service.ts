93d30749cb197b60c19d5150dd1ed4b4625c8d98
DalyChouikh <chouikhdaly215@gmail.com>
2024-01-02 00:33:47 +0100

    REFACTOR: Implemented basic crus operations on TODOS and USERS entities

--- src/users/user.service.ts
+++ src/users/user.service.ts
@@ -1,9 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
-import { Repository } from 'typeorm';
+import { ILike, Repository } from 'typeorm';
 import { Todo } from './entities/todo.entity';
-import { CreateTodoDto, CreateUserDto } from './dto';
+import {
+  CreateTodoDto,
+  CreateUserDto,
+  UpdateTodoDto,
+  UpdateUserDto,
+} from './dto';
+import { hash } from 'bcrypt';
 
 @Injectable()
 export class UserService {
@@ -12,20 +18,14 @@ export class UserService {
     @InjectRepository(Todo) private readonly todoRepository: Repository<Todo>,
   ) {}
 
-  async findAllTodos(id: string) {
-    return await this.todoRepository.find({
-      where: { id: +id },
-      relations: { user: true },
-    });
-  }
-
   async findAllUsers() {
     return await this.userRepository.find({
       relations: ['todos'],
     });
   }
 
-  createUser(createUserDto: CreateUserDto) {
+  async createUser(createUserDto: CreateUserDto) {
+    createUserDto.password = await hash(createUserDto.password, 10);
     const user = this.userRepository.create(createUserDto);
     return this.userRepository.save(user);
   }
@@ -34,5 +34,83 @@ export class UserService {
     const user = await this.userRepository.findOneBy({ id: +id });
     const todo = this.todoRepository.create({ ...createTodoDto, user });
     return this.todoRepository.save(todo);
+  }
+
+  async findUserById(id: string) {
+    const user = await this.userRepository.findOne({
+      where: { id: +id },
+      relations: ['todos'],
+    });
+    if (!user) throw new NotFoundException('User Not Found');
+    return user;
+  }
+
+  async findTodoById(userId: string, id: string) {
+    const user = await this.userRepository.findOneBy({ id: +userId });
+    if (!user) throw new NotFoundException('User Not Found');
+    const todo = await this.todoRepository.findOneBy({ id: +id, user });
+    if (!todo) throw new NotFoundException('Todo not found');
+    return todo;
+  }
+
+  async findTodoByTitle(id: string, title: string = '') {
+    const user = await this.userRepository.findOneBy({ id: +id });
+    if (!user) throw new NotFoundException('User Not Found');
+    return await this.todoRepository.find({
+      where: { title: ILike(`${title}%`), user },
+    });
+  }
+
+  async deleteTodoById(userId: string, id: string) {
+    const user = await this.userRepository.findOneBy({ id: +userId });
+    if (!user) throw new NotFoundException('User Not Found');
+    const todo = await this.todoRepository.findOneBy({ id: +id, user });
+    if (!todo) throw new NotFoundException('Todo not found');
+    await this.todoRepository.delete({ id: +id, user: user });
+    return { statusCode: 200, message: 'Todo deleted successfully' };
+  }
+
+  async updateTodoById(
+    userId: string,
+    id: string,
+    updateTodoDto: UpdateTodoDto,
+  ) {
+    const user = await this.userRepository.findOneBy({ id: +userId });
+    if (!user) throw new NotFoundException('User Not Found');
+    const todo = await this.todoRepository.preload({
+      id: +id,
+      userId: +userId,
+      ...updateTodoDto,
+    });
+    if (!todo) {
+      throw new NotFoundException('Todo not found');
+    }
+    return await this.todoRepository.save(todo);
+  }
+
+  async updateUser(id: string, updateUserDto: UpdateUserDto) {
+    if (updateUserDto.password) {
+      const newPassword = await hash(updateUserDto.password, 10);
+      updateUserDto.password = newPassword;
+    }
+    const user = await this.userRepository.preload({
+      id: +id,
+      ...updateUserDto,
+    });
+    if (!user) {
+      throw new NotFoundException('User Not Found');
+    }
+
+    return await this.userRepository.save(user);
+  }
+
+  async deleteUser(id: string) {
+    const user = await this.userRepository.findOne({
+      where: { id: +id },
+      relations: ['todos'],
+    });
+    if (!user) throw new NotFoundException('User Not Found');
+    await this.userRepository.delete({ id: +id });
+    return { statusCode: 200, message: 'User deleted successfully' };
   }
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,20 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { UserService } from './user.service';
-import { CreateTodoDto, CreateUserDto } from './dto';
+import {
+  CreateTodoDto,
+  CreateUserDto,
+  UpdateTodoDto,
+  UpdateUserDto,
+} from './dto';
 
 @Controller('users')
 export class UsersController {
@@ -19,5 +33,50 @@ export class UsersController {
   @Post(':id')
   createTodo(@Param('id') id: string, @Body() createTodoDto: CreateTodoDto) {
     return this.userService.createTodo(id, createTodoDto);
+  }
+
+  @Get(':id')
+  findUserById(@Param('id') id: string) {
+    return this.userService.findUserById(id);
+  }
+
+  @Get(':userId/todos/:id')
+  findTodoById(@Param('userId') userId: string, @Param('id') id: string) {
+    return this.userService.findTodoById(userId, id);
+  }
+
+  @Get(':id/todos')
+  findUserTodos(
+    @Param('id') id: string,
+    @Query() title: { title: string } = { title: '' },
+  ) {
+    return this.userService.findTodoByTitle(id, title.title);
+  }
+
+  @Delete(':userId/todos/:id')
+  deleteTodoById(@Param('userId') userId: string, @Param('id') id: string) {
+    return this.userService.deleteTodoById(userId, id);
+  }
+
+  @Patch(':userId/todos/:id')
+  updateTodoById(
+    @Param('userId') userId: string,
+    @Param('id') id: string,
+    @Body() updateTodoDto: UpdateTodoDto,
+  ) {
+    return this.userService.updateTodoById(userId, id, updateTodoDto);
+  }
+
+  @Patch(':id')
+  updateUserById(
+    @Param('id') id: string,
+    @Body() updateUserDto: UpdateUserDto,
+  ) {
+    return this.userService.updateUser(id, updateUserDto);
+  }
+
+  @Delete(':id')
+  deleteUserById(@Param('id') id: string) {
+    return this.userService.deleteUser(id);
   }
 }
